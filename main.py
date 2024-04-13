2335552af45063be6307337c16cc9da7f846f007
DipanshuGarg24 <166897791+DipanshuGarg24@users.noreply.github.com>
2024-04-13 13:42:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,45 @@ import streamlit as st
 
 # basic like title
 # text and some thing different
+#
+# st.title("Hi What are you doing write now :) ")
+# st.text("hi my name is dipanshu garg")
+#
+# x1 = list(st.columns(4))
+# x1[0].text("hii")
+# x1[1].text("hii")
+# x1[2].text("hii")
+# x1[3].text("hii")
+#
+# # x2.text("hello")
+#
+# # Lets Create that whatever i am thinking of
+# st.markdown("<H1>Hi my name is dipanshu garg </H1>",unsafe_allow_html=True)
 
-st.title("Hi What are you doing write now :) ")
-st.text("hi my name is dipanshu garg")
+def makeContent():
+    st.markdown("<h1 style='text-align: center;'>Computer Network</h1>", unsafe_allow_html=True)
+    st.markdown("<hr>",unsafe_allow_html=True)
 
-x1 = list(st.columns(4))
-x1[0].text("hii")
-x1[1].text("hii")
-x1[2].text("hii")
-x1[3].text("hii")
+    # making the coulmns
+    # lets say five coulmns
+    head = list(st.columns(5))
+    head[0].text("")
+    head[1].text("Concept")
+    head[2].text("1.Revision")
+    head[3].text("PYQS")
+    head[4].text("revision")
 
-# x2.text("hello")
+    x = list(st.columns(5))
+    x[0].text("IPV4 Adressing")
+    x[1].checkbox("",key=2)
+    x[2].checkbox("",key=3)
+    x[3].checkbox("",key=4)
 
-# Lets Create that whatever i am thinking of
-st.markdown("<H1>Hi my name is dipanshu garg </H1>",unsafe_allow_html=True)
+    y = list(st.columns(5))
+    y[0].text(" Error Control")
+    y[1].checkbox("", key=21)
+    y[2].checkbox("", key=31)
+    y[3].checkbox("", key=41)
+    topics = ["IPV4 Adressing","Error Control"]
 
-
+makeContent()
