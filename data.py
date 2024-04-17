b4c22b0e35cd597bee24cf61e0836b9297067c27
DipanshuGarg24 <166897791+DipanshuGarg24@users.noreply.github.com>
2024-04-17 18:52:35 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,17 @@
+SubList = [None,"C Programming","Data structure","Algorithm","Database System",
+                "Theorey of Computation","Compiler Design","Computer Network"
+               ,"Operating system","Computer Organization","Digital Logic",
+               "Engineering Maths","Discrete Maths","Aptitude"]
+# Subject wise topic list :)
+TopicList = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            ]
