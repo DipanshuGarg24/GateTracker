49c7cd322bf014d479ffd2a68961c27e385ef437
DipanshuGarg24 <166897791+DipanshuGarg24@users.noreply.github.com>
2024-05-15 00:13:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def local_css(file_name):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-local_css(".style/style.css")
+local_css("style.css")
 
 st.markdown("<h1 style='text-align:center'>🎯🎯 GATE 🎯🎯 </h1>",unsafe_allow_html=True)
 st.divider()
